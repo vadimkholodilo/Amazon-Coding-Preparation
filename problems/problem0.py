a43fe0542c20c974054a2730a075d598abfca95f
vadimkholodilo <vadimkholodilo@gmail.com>
2022-11-20 21:05:21 -0600

    Create an example for future problems

---
+++ problems/problem0.py
@@ -0,0 +1,5 @@
+# Solution for problem 0
+# You can find a description of problem0 in problem0.txt
+
+def solution(x):
+    return x
---
+++ problems/test_problem0.py
@@ -0,0 +1,4 @@
+from problem0 import solution
+
+def test_problem0():
+    assert solution(1) == 1
